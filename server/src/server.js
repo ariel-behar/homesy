40c3ff94e0ab858e1399fea193f4db82bdb94ba5
Ariel Behar <30868222+ariel-behar@users.noreply.github.com>
2021-11-20 16:47:50 +0200

    Changed server PORT to 3050

--- server/src/server.js
+++ server/src/server.js
@@ -6,7 +6,7 @@ const initDatabase = require('../config/initDatabase.js');
 
 const routes = require('./routes/routes.js')
 
-const PORT = process.env.PORT || '3030';
+const PORT = process.env.PORT || '3050';
 
 const app = express();
 
@@ -16,12 +16,14 @@ if(dbName && isNaN(Number(dbName))){
     initDatabase(dbName)
         .then(() => {
             console.log(chalk.green(`${chalk.green('Succesfully connected to database:')} ${chalk.white(dbName)}`))
-
+            
             app.listen(PORT, () => {
                 console.log(`${chalk.green('App is running on:')} http://localhost:${PORT}`);
             })
-        });
-
+        })
+        .catch(error => {
+            console.log(`An error occurred while connecting to database: ${error}`)
+        })
 }
 
 
