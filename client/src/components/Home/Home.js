42338d698a60d265c6c611c762f6f57ca28a2e32
Ariel Behar <30868222+ariel-behar@users.noreply.github.com>
2021-12-06 23:47:02 +0200

    Started building the request on the HOME page. Needs to be continued

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,12 +1,44 @@
 import styles from './Home.module.css';
+import typesOfServices from "../../data/typesOfServices.json";
+import SelectOptions from '../HomeServices/Create/SelectOptions/SelectOptions.js';
+import { useContext } from 'react';
+import ErrorContext from '../../contexts/ErrorContext.js';
+import * as homeServicesService from '../../services/homeServicesService.js';
 
 const Home = () => {
+    const { displayError } = useContext(ErrorContext);
+
+    const onFormSubmitHanlder = async (e) => {
+        e.preventDefault();
+
+        // TODO: THis part needs to be modified and all of the ones along the request chain
+
+        // const { typeOfService, cityOfOperation } = Object.fromEntries(new FormData(e.currentTarget));
+
+        // try {
+        //     let result = await homeServicesService.get(typeOfService, cityOfOperation);
+        //     console.log('result:', result)
+
+        // } catch(error) {
+        //     displayError(await error);
+        // }
+
+    }
+
     return (
         <section className={styles.homePageSection}>
             <p>Services</p>
-            <form method="POST" action="">
-                <input type="text" placeholder="What service?" />
-                <input type="text" placeholder="Where?" />
+            <form method="GET" action="" onSubmit={onFormSubmitHanlder}>
+                <select name="typeOfService" id="serviceType">
+                    {typesOfServices.map(x => {
+                        return (
+                            <SelectOptions key={x._id} value={x.value}>
+                                {x.name}
+                            </SelectOptions>
+                        );
+                    })}
+                </select>
+                <input type="text" name="cityOfOperation" placeholder="Where?" />
                 <input type="submit" />
             </form>
         </section>
--- client/src/services/homeServicesService.js
+++ client/src/services/homeServicesService.js
@@ -4,9 +4,12 @@ const homeServicesUrl = 'http://localhost:3030/home-services'
 
 export const create = (homeService, authToken) => request(`${homeServicesUrl}/create`, 'POST', authToken, homeService);
 
-export const getAll = () => request(homeServicesUrl, 'GET');
+export const getOne = homeServiceId => request(`${homeServicesUrl}/${homeServiceId}`, 'GET');
 
-export const getOne = (homeServiceId) => request(`${homeServicesUrl}/${homeServiceId}`, 'GET');
+//THIS one needs to be modified
+// export const get = (typeOfService, cityOfOperation) => request(homeServicesUrl, 'GET', null, { typeOfService, cityOfOperation });
+
+export const getAll = () => request(homeServicesUrl, 'GET');
 
 export const updateOne = (homeServiceId, homeService, authToken) => request(`${homeServicesUrl}/${homeServiceId}`, 'PUT', authToken, homeService);
 
