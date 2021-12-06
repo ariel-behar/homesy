a152a57d80ef981ce16c8c5858721a8e1bc97898
Ariel Behar <30868222+ariel-behar@users.noreply.github.com>
2021-12-06 23:55:22 +0200

    Update to display name with an Uppercase first letter in the header

--- client/src/components/Header/LoggedUserNavButtons/LoggedUserNavButtons.js
+++ client/src/components/Header/LoggedUserNavButtons/LoggedUserNavButtons.js
@@ -5,11 +5,11 @@ function LoggedUserNavButtons() {
     let { user } = useAuth()
 
     return (
-        <>  
-            <NavLink to="/home-services/create" > Create Listing </NavLink>
-            <span>Hello, {user.firstName}</span>
+        <>
+            <NavLink to="/home-services/create"> Create Listing </NavLink>
+            <span>Hello, {user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1).toLowerCase()}</span>
             <NavLink to="/logout"> Logout </NavLink>
         </>
-    )
+    );
 }
 export default LoggedUserNavButtons;
