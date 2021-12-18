14a5da96d0346ff50698f285405c8adf85731652
Ariel Behar <30868222+ariel-behar@users.noreply.github.com>
2021-12-18 17:39:58 +0200

    Creating a grid in the AllListings page

--- client/src/components/HomeServices/AllListings/AllListings.js
+++ client/src/components/HomeServices/AllListings/AllListings.js
@@ -1,27 +1,31 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { useErrorContext } from '../../../contexts/ErrorContext.js';
-import * as homeServicesService from '../../../services/homeServicesService.js'
+import * as homeServicesService from '../../../services/homeServicesService.js';
 
-// import styles from './AllListings.module.css'
+import styles from './AllListings.module.css'
 import ListingCard from './ListingCard/ListingCard.js';
 
 const Listings = () => {
-    const {displayError } = useErrorContext()
-    let [ services, setServices ] = useState([]);
+    const { displayError } = useErrorContext();
+    let [services, setServices] = useState([]);
 
     useEffect(() => {
-        homeServicesService.getAll()
+        homeServicesService
+            .getAll()
             .then(res => setServices(res))
-            .catch(err => displayError(err) )
-
+            .catch(err => displayError(err));
     }, []);
 
     return (
-        <div className="row">
-            {services.map(x => (
-                <ListingCard key={x._id} service={x} />
-            ))}
-        </div>
+        <section className={styles.allListingsSection}>
+            <h4>All Listings</h4>
+
+            <div className={styles['cardsDiv']}>
+                {services.map(x => (
+                    <ListingCard key={x._id} service={x} />
+                ))}
+            </div>
+        </section>
     );
 };
 
