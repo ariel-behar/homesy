64a5ab5452e3b6bc5feba77d495a323961d85a92
Ariel Behar <30868222+ariel-behar@users.noreply.github.com>
2021-12-22 10:21:20 +0200

    Modifying the 404 page

--- client/src/components/PageNotFound/PageNotFound.js
+++ client/src/components/PageNotFound/PageNotFound.js
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+
+import styles from './PageNotFound.module.scss';
+
 function PageNotFound() {
     return (
-        <div>
-            <h2>Page Not Found!</h2>
-        </div>
+        <section className={styles.pageNotFoundComponentSection}>
+            <h3>404 - Page Not Found!</h3>
+
+            <img src="/img/404-smiley.png" alt="404 Smiley"/>
+
+            <h4>Let's go <Link to="/"> HOME</Link> !</h4>
+            
+        </section>
     )
 }
 
