caad02c6f15b30a7694ed80a102b98560b026262
Ariel Behar <30868222+ariel-behar@users.noreply.github.com>
2022-03-26 22:22:25 +0200

    Migrating from a local to a cloud based database

--- server/config/initDatabase.js
+++ server/config/initDatabase.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 function initDatabase (dbName){
-    return mongoose.connect(`mongodb://localhost:27017/${dbName}`);
+    return mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wkrc6.mongodb.net/${dbName}`);
 }
 
 module.exports =  initDatabase;
